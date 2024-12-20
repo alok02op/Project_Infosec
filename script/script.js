3ecde83b5a294023207249e55a188a091fae5a5c
AlokKumar <alok.kumar.23033@iitgoa.ac.in>
2024-12-20 22:37:41 +0530

    updated style for phones-8

--- script/script.js
+++ script/script.js
@@ -29,3 +29,17 @@ document.addEventListener("DOMContentLoaded", () => {
 function clearForm(form) {
   setTimeout(() => {form.reset();}, 100);
 }
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href').substring(1);
+    const targetElement = document.getElementById(targetId);
+
+    if (targetElement) {
+      targetElement.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
